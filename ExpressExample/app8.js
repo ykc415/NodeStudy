407194e039e7d52df6e4376f0d546f93d8ae2eab
ykc <pica415@gmail.com>
2017-04-02 13:46:26 +0900

    요청 라우팅하기

---
+++ ExpressExample/app8.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+app.post('/process/login', function(req, res) {
+  console.log('/process/login 처리함.');
+
+  var paramId = req.param('id');
+  var paramPassword = req.param('password');
+
+  res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+  res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+  res.write('<div><p>Param id : ' + paramId + '</p></div>');
+  res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
+  res.write('<br><br><a href=\'/public/login2.html\'>로그인페이지로 돌아가기</a>');
+  res.end();
+
+});
+
+
+app.all('*', function(req, res) {
+  res.send(404, '<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>');
+});
+
+
+
+
+http.createServer(app).listen(3000, function() {
+  console.log('Express 서버가 3000번 포트에서 시작됨.');
+});
---
+++ ExpressExample/app9.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+// 오류 핸들러 모듈 사용
+var expressErrorHandler = require('express-error-handler');
+
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+app.post('/process/login', function(req, res) {
+  console.log('/process/login 처리함.');
+
+  var paramId = req.param('id');
+  var paramPassword = req.param('password');
+
+  res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+  res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+  res.write('<div><p>Param id : ' + paramId + '</p></div>');
+  res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
+  res.write('<br><br><a href=\'/public/login2.html\'>로그인페이지로 돌아가기</a>');
+  res.end();
+
+});
+
+
+// 모든 router 처리 끝난 후 404 오류 페이지 처리
+var errorHandler = expressErrorHandler({
+  static: {
+    '404': './public/404.html'
+  }
+});
+
+app.use( expressErrorHandler.httpError(404) );
+app.use( errorHandler );
+
+
+
+http.createServer(app).listen(3000, function() {
+  console.log('Express 서버가 3000번 포트에서 시작됨.');
+});
